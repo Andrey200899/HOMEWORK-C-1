aaa7982e1e88b90dd5c5b3bca96f220bf22ba03a
unknown <kvkvachev@yandex.ru>
2022-11-12 20:27:19 +0300

    EX 3

--- Program.cs
+++ Program.cs
@@ -40,3 +40,17 @@ else
 {
     Console.WriteLine("Максимальное число: " + C);
 }
+
+
+
+
+Console.WriteLine("Введи  число:");
+int number = int.Parse(Console.ReadLine()!);
+if (number % 2==0)
+{
+    Console.WriteLine("да");
+}
+else 
+{
+     Console.WriteLine("нет");
+}
