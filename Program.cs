fb428e5bd05e1da465ad5410453ebd279f70fb94
unknown <kvkvachev@yandex.ru>
2022-11-12 20:06:43 +0300

    EX №2

--- Program.cs
+++ Program.cs
@@ -10,3 +10,33 @@ else
 {
     Console.WriteLine($"{b}=max");
 }
+
+
+
+
+Console.WriteLine("Введи первое число:");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введи второе число:");
+int B = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введи третье число:");
+int C = int.Parse(Console.ReadLine()!);
+if (A > B)
+{
+    if (A > C)
+    {
+        Console.WriteLine("Максимальное число: " + A);
+    }
+    else
+    {
+        Console.WriteLine("Максимальное число: " + C);
+    }
+}
+
+else if (B > C)
+{
+    Console.WriteLine("Максимальное число: " + B);
+}
+else
+{
+    Console.WriteLine("Максимальное число: " + C);
+}
