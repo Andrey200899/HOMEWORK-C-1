c0116eb3a31f8d360e778282223ee43990061ce3
unknown <kvkvachev@yandex.ru>
2022-11-12 19:03:50 +0300

    Ex №1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введи первое число:");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введи второе число:");
+int b = int.Parse(Console.ReadLine()!);
+if (a>b)
+{
+    Console.WriteLine($"{a}=max");
+}
+else
+{
+    Console.WriteLine($"{b}=max");
+}
