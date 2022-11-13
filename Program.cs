77be469f3149fd83229ef8bde7edcd3727b04e47
unknown <kvkvachev@yandex.ru>
2022-11-13 10:44:00 +0300

    EX 4

--- Program.cs
+++ Program.cs
@@ -54,3 +54,17 @@ else
 {
      Console.WriteLine("нет");
 }
+
+
+
+  Console.Write("Введите первое число: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.Write("Введите второе число: ");
+        int m = int.Parse(Console.ReadLine());
+        for (int i = n; i <= m; i++)
+        {
+            if (i % 2 == 0) 
+            {
+                Console.Write(i + " ");
+            }
+        }
